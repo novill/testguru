fab0d483bbd236f8eb4838df95ff8ac3b05c1149
Alexey Shemyakin <2alexeysh@gmail.com>
2019-02-18 13:53:31 +0300

    validations_and_scopes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,13 @@
 class Question < ApplicationRecord
   belongs_to :test
   has_many :answers
+
+  validates :body, presence: true
+  validates :answers_count, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 4}
+
+  def answers_count
+    answers.count
+  end
+  # можно наверно через validate и вызов метода, но это длинно
+
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -5,16 +5,33 @@ class Test < ApplicationRecord
   has_many :passing_users, through: :results, source: :users
 
   belongs_to :author, class_name: 'User', foreign_key: :user_id
+
+  validates :title, presence: true
+  validates :level, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  # Может существовать только один Тест с данным названием и уровнем
+  validates :title, uniqueness: { scope: :level }
+
+  scope :by_hardness, -> (hardness) do
+    levels = case hardness
+             when :easy then 0..1
+             when :medium then 2..4
+             when :hard then 5.. Float::INFINITY
+             end
+    where(level: levels)
+  end
+
   # Создайте метод класса в модели Test,
   # который возвращает отсортированный по убыванию массив названий всех Тестов
   # у которых Категория называется определённым образом
   # (название категории передается в метод в качестве аргумента).
+  scope  :by_category_name, -> (name) do
+    joins(:category)
+    .where(categories: { title: name })
+    .order('tests.title DESC')
+  end
 
   def self.titles_by_category_name(name)
-    Test.joins(:category)
-        .where(categories: { title: name })
-        .order('tests.title DESC')
-        .pluck('tests.title')
+    by_category_name(name).pluck('tests.title')
   end
 
   def to_s
