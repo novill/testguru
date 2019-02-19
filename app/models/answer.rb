244c8ba567230855974a5e008de6246e22ed9e16
Alexey Shemyakin <2alexeysh@gmail.com>
2019-02-19 22:25:48 +0300

    Remove comments

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,10 +2,6 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-
-  # При добавлении нового ответа их должно быть меньше 4
-  # validates :question_answers_count, numericality: { less_than: 4 }, on: :create
-
   validate :validate_question_answers_count, on: :create
 
   scope :correct, -> { where(correct: true) }
@@ -15,8 +11,4 @@ class Answer < ApplicationRecord
     def validate_question_answers_count
       errors.add(:question, 'Too much answers') if question.answers.count >= 4
     end
-
-  # def question_answers_count
-  #   question.answers.count
-  # end
 end
