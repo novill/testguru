d23f7500c9de9a58ff700aeadad3c8a6e5ef1ce6
Alexey Shemyakin <2alexeysh@gmail.com>
2019-02-20 17:55:10 +0300

    Просмотра конкретного вопроса теста

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,7 +6,15 @@ class QuestionsController < ApplicationController
     render plain: @test.questions.map(&:body).join("\n")
   end
 
+  def show
+    set_question
+    render plain: @question.body
+  end
+
 private
+  def set_question
+    @question = @test.questions.find(params[:id])
+  end
 
   def set_test
     @test = Test.find(params[:test_id])
