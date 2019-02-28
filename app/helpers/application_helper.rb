ae7616ff4bb42beaf7820854b445e619fc40d456
Alexey Shemyakin <2alexeysh@gmail.com>
2019-02-28 13:46:24 +0300

    Views update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,10 +6,4 @@ module ApplicationHelper
   def current_year
     Time.current.year
   end
-
-  def flash_message(name)
-    if flash.any?
-      content_tag :p, flash[name], class: "flash #{name}"
-    end
-  end
 end
