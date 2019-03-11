67143d9c26ae2efab1c2d5a53c69d4388eb964e4
Alexey Shemyakin <2alexeysh@gmail.com>
2019-03-11 23:35:23 +0300

    Fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'mighty-eyrie-86451.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp+settings = {
+  config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
     username: ENV['SMTP_USERNAME'],
